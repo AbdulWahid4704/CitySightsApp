06e74c35fa6f8a47b506223ea3c978601842dd98
AbdulWahid4704 <83956387+AbdulWahid4704@users.noreply.github.com>
2022-01-05 22:19:32 +0400

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Rusaab Abdul Wahid on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
